7775361cbeafe6b00253f64b7c1d8e6f0bd7e85b
Roberto Antonio Alferes Gomez <ag1961012015@unab.edu.sv>
2019-12-12 16:29:07 -0600

    modificacion de la base de datos

--- restaurante.sql
+++ restaurante.sql
@@ -6,27 +6,31 @@ use restaurante;
 create table usuario(
 	id_usuario int primary key auto_increment,
     usuario varchar(50),
-    password varchar(50)
+    clave varchar(50),
+    estado int(1) not null default 0
 );
 -- perfil cambia a roles
 create table roles(
 	id_perfil int primary key auto_increment,
-    nombre varchar(50)
+    nombre varchar(50),
+    estado int(1) not null default 0
 );
 
 create table usuario_roles(
 	id_usu_per int primary key auto_increment,
     id_usuario int,
     id_perfil int,
+    estado int(1) not null default 0,
     constraint fk_up_usu foreign key (id_usuario) references usuario(id_usuario),
-    constraint fk_up_per foreign key (id_perfil) references perfil(id_perfil)
+    constraint fk_up_per foreign key (id_perfil) references roles(id_perfil)
 );
 
 -- tablas para sucursales y empleados
 
 create table tipo_sucursal(
 	id_tipo_sucursal int primary key auto_increment,
-    nombre varchar(50)
+    nombre varchar(50),
+    estado int(1) not null default 0
 );
 
 create table sucursal(
@@ -35,12 +39,14 @@ create table sucursal(
     direccion varchar(50),
     telefono varchar(10),
     id_tipo_sucursal int,
+    estado int(1) not null default 0,
     constraint fk_suc_tipsuc foreign key (id_tipo_sucursal) references tipo_sucursal(id_tipo_sucursal)
 );
 
 create table tipo_empleado(
 	id_tipo_empleado int primary key auto_increment,
-    nombre varchar(50)
+    nombre varchar(50),
+    estado int(1) not null default 0
 );
 
 create table empleado(
@@ -51,6 +57,7 @@ create table empleado(
     telefono varchar(10),
     id_tipo_empleado int,
     id_sucursal int,
+    estado int(1) not null default 0,
     constraint fk_emp_tem foreign key (id_tipo_empleado) references tipo_empleado(id_tipo_empleado),
     constraint fk_emp_suc foreign key (id_sucursal) references sucursal(id_sucursal)
 );
@@ -60,6 +67,7 @@ create table credenciales_empleado(
 	id_cre_emp int primary key auto_increment, 
     id_empleado int,
     id_usu_per int,
+    estado int(1) not null default 0,
     constraint fk_ce_emp foreign key (id_empleado) references empleado(id_empleado),
     constraint fk_ce_usuper foreign key (id_usu_per) references usuario_perfil(id_usu_per)
 );
@@ -70,13 +78,15 @@ create table proveedor(
 	id_proveedor int primary key auto_increment,
     	nombre varchar(50),
     	direccion varchar(100),
-    	telefono varchar(10)
+    	telefono varchar(10),
+        estado int(1) not null default 0
 );
 
 -- ejem: desayuno, almuerzo, cena, entradas, tipicos, bebida fria, bebida caliente
 create table tipo_producto(
 	id_tipo_producto int primary key auto_increment,
-    	nombre varchar(50)
+    	nombre varchar(50),
+        estado int(1) not null default 0
 );
 
 -- ejem: huevos, carne, tamal pisque, sopa esparrago, empanada, soda, cafe, chocolate
@@ -84,19 +94,22 @@ create table producto(
 	id_producto int primary key auto_increment,
     	nombre varchar(50),
     	id_tipo_producto int,
+        estado int(1) not null default 0,
     	constraint fk_pro_tp foreign key (id_tipo_producto) references tipo_producto(id_tipo_producto)
 );
 
 -- ejem: tomatada, guisada, salsita, frijol,  
 create table especialidad(
 	id_especialidad int primary key auto_increment,
-    	nombre varchar(50)
+    	nombre varchar(50),
+        estado int(1) not null default 0
 );
 
 create table producto_especialidad(
 	id_pro_esp int primary key auto_increment,
     	id_producto int,
     	id_especialidad int,
+        estado int(1) not null default 0,
     	constraint fk_pe_pro foreign key (id_producto) references producto(id_producto),
     	constraint fk_pe_esp foreign key (id_especialidad) references especialidad(id_especialidad)
 );
@@ -108,19 +121,22 @@ create table proveedor_producto_sucursal(
     	id_sucursal int,
     	cantidad int,
     	precio_proveedor decimal(12,5),
+        estado int(1) not null default 0,
     	constraint fk_pps_prov foreign key (id_proveedor) references proveedor(id_proveedor),
     	constraint fk_pps_prod foreign key (id_producto) references producto(id_producto),
     	constraint fk_pps_suc foreign key (id_sucursal) references sucursal(id_sucursal)
 );
 
 -- tablas para realizar la venta
---inventario
+-- inventario
 create table inventario (
 	id_stock int primary key auto_increment,
    --  id_pro_esp int, 
     id_sucursal int,
     cant_disponible int, 
+    cant_entrada int,
     precio_unitario decimal(12,5),
+    estado int(1) not null default 0,
     -- constraint fk_sto_proesp foreign key (id_pro_esp) references producto_especialidad(id_pro_esp),
     constraint fk_sto_suc foreign key (id_sucursal) references sucursal(id_sucursal)
 );
@@ -131,7 +147,10 @@ create table venta(
     -- id_sucursal int,
     venta_total decimal(12,5),
     fecha_venta date,
-    constraint fk_ven_emp foreign key (id_empleado) references empleado(id_empleado),
+    subtotal decimal(10,2),
+    venta_hora time,
+    estado int(1) not null default 0,
+    constraint fk_ven_emp foreign key (id_empleado) references empleado(id_empleado)
    --  constraint fk_ven_suc foreign key (id_sucursal) references sucursal(id_sucursal)
 );
 
@@ -140,6 +159,7 @@ create table venta_detalle(
     id_venta int,
     id_pro_esp int, 
     cantidad int,
+    estado int(1) not null default 0,
     constraint fk_vd_proesp foreign key (id_pro_esp) references producto_especialidad(id_pro_esp),
     constraint fk_vd_ven foreign key (id_venta) references venta(id_venta)
 );
@@ -148,6 +168,7 @@ create table menu_del_dia(
 	id_menu_del_dia int primary key auto_increment,
     id_pro_esp int,
     descuento int,
+    estado int(1) not null default 0,
     constraint fk_mdd_proesp foreign key (id_pro_esp) references producto_especialidad(id_pro_esp)
 );
 
