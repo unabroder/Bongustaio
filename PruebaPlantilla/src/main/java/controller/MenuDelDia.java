30b059fe03bd4b358f4dc2a1d8d706abf799547c
nathaliaflores <nathaliaflores17@gmail.com>
2019-12-18 11:31:33 -0600

    menuDelDia

---
+++ PruebaPlantilla/src/main/java/controller/MenuDelDia.java
@@ -0,0 +1,17 @@
+
+package controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+
+@Named(value = "menuDelDia")
+@SessionScoped
+public class MenuDelDia implements Serializable {
+
+  
+    public MenuDelDia() {
+    }
+    
+}
