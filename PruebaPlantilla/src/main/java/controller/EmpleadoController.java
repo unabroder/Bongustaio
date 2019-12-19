a8e31730b0037325417701635407840b4c540563
Roberto Antonio Alferes Gomez <ag1961012015@unab.edu.sv>
2019-12-19 16:41:04 -0600

    se completo el controlador de empleado

--- PruebaPlantilla/src/main/java/controller/EmpleadoController.java
+++ PruebaPlantilla/src/main/java/controller/EmpleadoController.java
@@ -6,7 +6,11 @@
 package controller;
 
 import ejb.EmpleadoFacadeLocal;
+import ejb.SucursalFacadeLocal;
+import ejb.TipoEmpleadoFacadeLocal;
 import entity.Empleado;
+import entity.Sucursal;
+import entity.TipoEmpleado;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -21,28 +25,71 @@ import javax.inject.Named;
 @Named(value = "empleadoController")
 @SessionScoped
 public class EmpleadoController implements Serializable {
-    
+
     @EJB
     private EmpleadoFacadeLocal empleadoEJB;
     private Empleado empleado;
     private List<Empleado> listaEmpleado;
-    
-    public EmpleadoFacadeLocal getEmpleadoEJB() {
-        return empleadoEJB;
-    }
-    
-    public void setEmpleadoEJB(EmpleadoFacadeLocal empleadoEJB) {
-        this.empleadoEJB = empleadoEJB;
-    }
-    
+
+    private SucursalFacadeLocal sucursalEJB;
+    private Sucursal sucursal;
+    private List<Sucursal> listSucursal;
+
+    private TipoEmpleadoFacadeLocal tipoEmpEJB;
+    private TipoEmpleado tipoEmp;
+    private List<TipoEmpleado> lsTipoEmp;
+    //Producto == pro
+    //porveedor == prov
+    //lista = lis
+
     public Empleado getEmpleado() {
         return empleado;
     }
-    
+
     public void setEmpleado(Empleado empleado) {
         this.empleado = empleado;
     }
-    
+
+    public List<Empleado> getListaEmpleado() {
+        return listaEmpleado;
+    }
+
+    public void setListaEmpleado(List<Empleado> listaEmpleado) {
+        this.listaEmpleado = listaEmpleado;
+    }
+
+    public Sucursal getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(Sucursal sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public List<Sucursal> getListSucursal() {
+        return listSucursal;
+    }
+
+    public void setListSucursal(List<Sucursal> listSucursal) {
+        this.listSucursal = listSucursal;
+    }
+
+    public TipoEmpleado getTipoEmp() {
+        return tipoEmp;
+    }
+
+    public void setTipoEmp(TipoEmpleado tipoEmp) {
+        this.tipoEmp = tipoEmp;
+    }
+
+    public List<TipoEmpleado> getLsTipoEmp() {
+        return lsTipoEmp;
+    }
+
+    public void setLsTipoEmp(List<TipoEmpleado> lsTipoEmp) {
+        this.lsTipoEmp = lsTipoEmp;
+    }
+
     @PostConstruct
     public void init() {
         empleado = new Empleado();
@@ -50,19 +97,48 @@ public class EmpleadoController implements Serializable {
 
     public void insertar() {
         try {
+            empleado.setIdsucursal(sucursal);
+            empleado.setIdtipoempleado(tipoEmp);
             empleadoEJB.create(empleado);
         } catch (Exception e) {
         }
     }
-    
-    public void editar(){
+
+    public void editar() {
         try {
+            empleado.setIdsucursal(sucursal);
+            empleado.setIdtipoempleado(tipoEmp);
             empleadoEJB.edit(empleado);
         } catch (Exception e) {
         }
     }
-    
-    public void eliminar(){
-        
+
+    public void eliminar(Empleado emp) {
+        try {
+            empleado = emp;
+            empleadoEJB.remove(empleado);
+        } catch (Exception e) {
+        }
+    }
+
+    public void lisEmp() {
+        try {
+            listaEmpleado = empleadoEJB.findAll();
+        } catch (Exception e) {
+        }
+    }
+
+    public void lsSucursal() {
+        try {
+            listSucursal = sucursalEJB.findAll();
+        } catch (Exception e) {
+        }
+    }
+
+    public void lisTipoEmp() {
+        try {
+            lsTipoEmp = tipoEmpEJB.findAll();
+        } catch (Exception e) {
+        }
     }
 }
