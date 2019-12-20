15b8270935084ba6871035b2c1d0198348f8d39f
Roberto Antonio Alferes Gomez <ag1961012015@unab.edu.sv>
2019-12-20 13:33:17 -0600

    cambios de josue

--- PruebaPlantilla/target/generated-sources/annotations/entity/Sucursal_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Sucursal_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-19T16:26:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T13:28:54")
 @StaticMetamodel(Sucursal.class)
 public class Sucursal_ { 
 
--- PruebaPlantilla/target/generated-sources/annotations/entity/TipoEmpleado_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/TipoEmpleado_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-19T16:26:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T13:28:54")
 @StaticMetamodel(TipoEmpleado.class)
 public class TipoEmpleado_ { 
 
