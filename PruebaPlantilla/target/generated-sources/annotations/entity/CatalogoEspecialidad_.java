e28dc1027e6f102ef2e4d128bfec4df98e8d96da
nathaliaflores <nathaliaflores17@gmail.com>
2019-12-20 11:10:58 -0600

    cambio menudeldiaControler

--- PruebaPlantilla/target/generated-sources/annotations/entity/CatalogoEspecialidad_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/CatalogoEspecialidad_.java
@@ -6,11 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T10:31:46")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T09:25:08")
->>>>>>> d74d99cad30f52334f5e3c1b4c9586fdab6f782a
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T10:37:16")
 @StaticMetamodel(CatalogoEspecialidad.class)
 public class CatalogoEspecialidad_ { 
 
