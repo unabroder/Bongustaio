ca36dd0419b1c003b17f2b417582a46000d194e2
Roberto Antonio Alferes Gomez <ag1961012015@unab.edu.sv>
2019-12-20 08:19:08 -0600

    controlador de empleado

--- PruebaPlantilla/src/main/java/controller/InventarioController.java
+++ PruebaPlantilla/src/main/java/controller/InventarioController.java
@@ -8,11 +8,11 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.SessionScoped;
 
 
 @Named(value = "inventarioController")
-@Dependent
+@SessionScoped
 public class InventarioController implements Serializable{
     
     //Declarar variables
