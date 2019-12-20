9ab7f996cb01a4b102bccb94e28033c2681b8b40
Claudiita <clau333.santos@gmail.com>
2019-12-20 11:07:39 -0600

    controller de proveedorproductofacadelocal corregdo

--- PruebaPlantilla/target/generated-sources/annotations/entity/Especialidad_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Especialidad_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T09:25:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T10:49:05")
 @StaticMetamodel(Especialidad.class)
 public class Especialidad_ { 
 
