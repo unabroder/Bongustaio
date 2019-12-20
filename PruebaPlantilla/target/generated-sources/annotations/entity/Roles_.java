0a607147d9bd3f3b105766bcd2cfda1f3084cd8f
Claudiita <clau333.santos@gmail.com>
2019-12-20 13:34:54 -0600

    actualizacion

--- PruebaPlantilla/target/generated-sources/annotations/entity/Roles_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Roles_.java
@@ -4,19 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T10:49:05")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T10:31:46")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T09:25:09")
->>>>>>> d74d99cad30f52334f5e3c1b4c9586fdab6f782a
->>>>>>> a53346a7d956554d3e619ff4b236af441eb9202d
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T10:37:16")
->>>>>>> e28dc1027e6f102ef2e4d128bfec4df98e8d96da
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T11:23:47")
 @StaticMetamodel(Roles.class)
 public class Roles_ { 
 
