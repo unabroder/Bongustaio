d74d99cad30f52334f5e3c1b4c9586fdab6f782a
Claudiita <clau333.santos@gmail.com>
2019-12-20 10:15:42 -0600

    agregar

--- PruebaPlantilla/target/generated-sources/annotations/entity/Venta_Detalle_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Venta_Detalle_.java
@@ -6,11 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-19T16:12:13")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-19T16:26:29")
->>>>>>> 5217dee804590ed8bdceda5231868ff3fbf5bc28
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-20T09:25:08")
 @StaticMetamodel(Venta_Detalle.class)
 public class Venta_Detalle_ { 
 
