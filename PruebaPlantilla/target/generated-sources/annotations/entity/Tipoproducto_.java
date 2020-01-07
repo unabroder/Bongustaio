dc1d853523df44e446cf243e42d46a7e297542b7
Roberto Antonio Alferes Gomez <ag1961012015@unab.edu.sv>
2020-01-07 14:42:07 -0600

    cambios en inventario

--- PruebaPlantilla/target/generated-sources/annotations/entity/Tipoproducto_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Tipoproducto_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T13:27:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T13:40:00")
 @StaticMetamodel(Tipoproducto.class)
 public class Tipoproducto_ { 
 
