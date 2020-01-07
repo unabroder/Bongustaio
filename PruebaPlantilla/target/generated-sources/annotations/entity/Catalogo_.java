d37cf1f8debfcd2533655ae303317f5b757aa858
renelopez <renefrancisco1993lopez@hotmail.com>
2020-01-07 14:18:53 -0600

    actalizando platocompleto

--- PruebaPlantilla/target/generated-sources/annotations/entity/Catalogo_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Catalogo_.java
@@ -5,17 +5,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T11:44:52")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T13:27:50")
->>>>>>> 6fc820610add1e897df34a1384f0abf439f38469
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T14:13:31")
 @StaticMetamodel(Catalogo.class)
 public class Catalogo_ { 
 
-    public static volatile SingularAttribute<Catalogo, Double> preciounit;
     public static volatile SingularAttribute<Catalogo, Integer> idcatalogo;
-    public static volatile SingularAttribute<Catalogo, Integer> cantidad;
     public static volatile SingularAttribute<Catalogo, String> nombre;
     public static volatile SingularAttribute<Catalogo, Tipoproducto> idtipoproduc;
 
--- PruebaPlantilla/target/generated-sources/annotations/entity/Proveedor_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Proveedor_.java
@@ -4,11 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T11:44:52")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T13:27:50")
->>>>>>> 6fc820610add1e897df34a1384f0abf439f38469
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T14:13:31")
 @StaticMetamodel(Proveedor.class)
 public class Proveedor_ { 
 
