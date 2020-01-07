6fc820610add1e897df34a1384f0abf439f38469
Roberto Antonio Alferes Gomez <ag1961012015@unab.edu.sv>
2020-01-07 13:29:23 -0600

    cambios en ordencompra

--- PruebaPlantilla/target/generated-sources/annotations/entity/Inventario_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Inventario_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T11:28:00")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T13:27:50")
 @StaticMetamodel(Inventario.class)
 public class Inventario_ { 
 
