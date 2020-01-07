aa6855cbe3b45f965fec909d8d2aea25f2e6307d
nathaliaflores <nathaliaflores17@gmail.com>
2020-01-07 10:40:52 -0600

    menudeldia

--- PruebaPlantilla/target/generated-sources/annotations/entity/Inventario_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Inventario_.java
@@ -5,11 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T09:29:20")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-06T16:29:59")
->>>>>>> 328645d1e25083079688b013e106caad6e0f63ce
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T10:31:22")
 @StaticMetamodel(Inventario.class)
 public class Inventario_ { 
 
