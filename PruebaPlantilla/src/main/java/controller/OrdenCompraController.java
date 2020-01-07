b958a6eb03f262dd2f9ad72c64f85c63f0303681
Roberto Antonio Alferes Gomez <ag1961012015@unab.edu.sv>
2020-01-07 13:25:50 -0600

    cambios de OrdenCompra

--- PruebaPlantilla/src/main/java/controller/OrdenCompraController.java
+++ PruebaPlantilla/src/main/java/controller/OrdenCompraController.java
@@ -1,44 +1,47 @@
-
 package controller;
 
-import ejb.OrdenCompraFacade;
 import ejb.OrdenCompraFacadeLocal;
-import ejb.ProductoFacade;
 import ejb.ProductoFacadeLocal;
 import ejb.ProveedorFacadeLocal;
+import ejb.SucursalFacadeLocal;
 import entity.OrdenCompra;
 import entity.Producto;
 import entity.Proveedor;
+import entity.Sucursal;
 import java.io.Serializable;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @ManagedBean
 @SessionScoped
 @Named(value = "ordencompracontroller")
-public class OrdenCompraController implements Serializable{
-    
+public class OrdenCompraController implements Serializable {
+
     @EJB
     private OrdenCompraFacadeLocal OrdenEJB;
     private OrdenCompra ordencompra;
     private List<OrdenCompra> listaorden;
-    
+
     @EJB
     private ProveedorFacadeLocal proveedorEJB;
     private Proveedor proveedor;
     private List<Proveedor> listaProveedor;
-    
+
+    @EJB
+    private SucursalFacadeLocal sucursalEJB;
+    private Sucursal sucursal;
+    private List<Sucursal> listaSucursal;
+
     @EJB
     private ProductoFacadeLocal productoEJB;
     private Producto producto;
     private List<Producto> listaproducto;
-
-    public OrdenCompra getOrdencompra() {
-        return ordencompra;
-    }
 
     public void setOrdencompra(OrdenCompra ordencompra) {
         this.ordencompra = ordencompra;
@@ -50,6 +53,7 @@ public class OrdenCompraController implements Serializable{
 
     public void setListaorden(List<OrdenCompra> listaorden) {
         this.listaorden = listaorden;
+
     }
 
     public Proveedor getProveedor() {
@@ -68,6 +72,22 @@ public class OrdenCompraController implements Serializable{
         this.listaProveedor = listaProveedor;
     }
 
+    public Sucursal getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(Sucursal sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public List<Sucursal> getListaSucursal() {
+        return listaSucursal;
+    }
+
+    public void setListaSucursal(List<Sucursal> listaSucursal) {
+        this.listaSucursal = listaSucursal;
+    }
+
     public Producto getProducto() {
         return producto;
     }
@@ -84,7 +104,90 @@ public class OrdenCompraController implements Serializable{
         this.listaproducto = listaproducto;
     }
 
-   
-    
-            
+    @PostConstruct
+    private void init() {
+        ordencompra = new OrdenCompra();
+        proveedor = new Proveedor();
+        sucursal = new Sucursal();
+        producto = new Producto();
+    }
+
+    public void consultarProvedor() {
+        try {
+            listaProveedor = proveedorEJB.findAll();
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void consultarSucursal() {
+        try {
+            listaSucursal = sucursalEJB.findAll();
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void consultarProducto() {
+        listaproducto = productoEJB.findAll();
+    }
+
+    public void insertar() {
+        try {
+            ordencompra.setIdproveedor(proveedor);
+            ordencompra.setIdproducto(producto);
+
+            OrdenEJB.create(ordencompra);
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "su registro fue guardado", null);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void listar() {
+        try {
+            listaorden = OrdenEJB.findAll();
+
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void modificar() {
+        try {
+            ordencompra.setIdproveedor(proveedor);
+            ordencompra.setIdproducto(producto);
+            OrdenEJB.edit(ordencompra);
+
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void leerid(OrdenCompra orden) {
+        try {
+            this.proveedor.setIdproveedor(orden.getIdproveedor().getIdproveedor());
+            this.producto.setIdproducto(orden.getIdproducto().getIdproducto());
+            this.ordencompra = orden;
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void eliminar(OrdenCompra orden) {
+        try {
+            ordencompra = orden;
+            OrdenEJB.remove(ordencompra);
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void limpiar() {
+        ordencompra = new OrdenCompra();
+
+    }
+
 }
