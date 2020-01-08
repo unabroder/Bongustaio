aadfe1578f42d14d16b6602b4327d95ac64d1cd8
nathaliaflores <nathaliaflores17@gmail.com>
2020-01-08 16:06:28 -0600

    Cambio Jodido

--- PruebaPlantilla/target/generated-sources/annotations/entity/Inventario_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Inventario_.java
@@ -5,15 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T11:04:42")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T11:45:14")
->>>>>>> dc6bd176e326032359faf4b5e52280cc035ad75a
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T10:38:07")
->>>>>>> c4c91e415bf0eee1c73ab9ab7acdab179450e70c
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T16:03:22")
 @StaticMetamodel(Inventario.class)
 public class Inventario_ { 
 
