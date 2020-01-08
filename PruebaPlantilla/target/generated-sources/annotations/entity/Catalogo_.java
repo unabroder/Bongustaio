c4c91e415bf0eee1c73ab9ab7acdab179450e70c
Josue Alexander Vasquez Escobar <Josueescobar080497@gmail.com>
2020-01-08 10:40:22 -0600

    cambios de josue

--- PruebaPlantilla/target/generated-sources/annotations/entity/Catalogo_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Catalogo_.java
@@ -5,11 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T16:32:15")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T09:45:50")
->>>>>>> f82e51074085a7c930ab663f079189b88c869884
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T10:38:07")
 @StaticMetamodel(Catalogo.class)
 public class Catalogo_ { 
 
