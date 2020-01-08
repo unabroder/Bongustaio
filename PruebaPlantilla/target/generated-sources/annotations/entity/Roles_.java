dbe69ab036f0801288302fc0927b7235447423c7
nathaliaflores <nathaliaflores17@gmail.com>
2020-01-08 16:22:52 -0600

    Agregar Cambio

--- PruebaPlantilla/target/generated-sources/annotations/entity/Roles_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Roles_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T16:03:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-08T16:10:52")
 @StaticMetamodel(Roles.class)
 public class Roles_ { 
 
