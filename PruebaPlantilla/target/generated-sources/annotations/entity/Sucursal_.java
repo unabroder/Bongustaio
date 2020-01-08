d5f846ed4029e14ae7502029f0df28b777dad3aa
Joqtan Alberto Marmol Lazo <joqtan.marmolusam@sv.cds>
2020-01-08 08:10:23 -0600

    avance

--- PruebaPlantilla/target/generated-sources/annotations/entity/Sucursal_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Sucursal_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T11:28:00")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-07T11:45:32")
 @StaticMetamodel(Sucursal.class)
 public class Sucursal_ { 
 
