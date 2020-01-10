f465409059826f68c0405dd3d278305b6105e3f4
Juan Carlos Ruiz Nativi <Juanativi95@gmail.com>
2020-01-10 09:22:19 -0600

    push de nativi

--- PruebaPlantilla/target/generated-sources/annotations/entity/Tiposucursal_.java
+++ PruebaPlantilla/target/generated-sources/annotations/entity/Tiposucursal_.java
@@ -4,19 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-09T16:21:38")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-09T11:37:33")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-09T11:32:19")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-09T14:44:10")
->>>>>>> 666ae4cb0f8a79a0675b386bf6414e6d813983bf
->>>>>>> 2f88033648ba47d4dfc40f096d37cdcaa4c3bdcd
->>>>>>> 777c6fc30e264b0c3f6662b32c03c20b2977cf56
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-10T09:19:02")
 @StaticMetamodel(Tiposucursal.class)
 public class Tiposucursal_ { 
 
